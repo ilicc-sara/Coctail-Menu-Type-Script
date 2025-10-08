a2ba6c4d637017ce04f5b3412f39b6923e192183
Sara <sarailic431@gmail.com>
2025-10-08 22:21:49 +0200

    fetched coctail api and installed tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(
+          "https://thecocktaildb.com/api/json/v1/1/randomselection.php"
+          // https://thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
+        );
+        const posts = await response.json();
+        console.log(posts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPost();
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-red-500">Coctail Menu</h1>
+    </>
+  );
 }
 
 export default App;
