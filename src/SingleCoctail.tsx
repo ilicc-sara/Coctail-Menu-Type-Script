49c06d092ee6841610400b695344e73d919a863d
Sara <sarailic431@gmail.com>
2025-10-12 19:58:32 +0200

    loading single coctail details page

--- src/SingleCoctail.tsx
+++ src/SingleCoctail.tsx
@@ -1,15 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 function SingleCoctail() {
+  type SnglCoctail = {
+    idDrink: string;
+    strAlcoholic: string;
+    strDrink: string;
+    strDrinkThumb: string;
+    strIngredient1: string;
+    strIngredient2: string;
+    strIngredient3?: string;
+    strIngredient4?: string;
+    strIngredient5?: string;
+    strIngredient6?: string;
+    strIngredient7?: string;
+  };
+
+  const [coctail, setCoctail] = useState<SnglCoctail[]>([]);
   const params = useParams();
-  console.log(params.coctailID);
 
   useEffect(() => {
     const fetchPost = async () => {
       try {
         const response = await fetch(
-          `https://thecocktaildb.com/api/json/v1/1/lookup.php?i=11007`
+          `https://thecocktaildb.com/api/json/v1/1/lookup.php?i=${params.coctailID}`
         );
         const posts = await response.json();
         console.log(posts);
